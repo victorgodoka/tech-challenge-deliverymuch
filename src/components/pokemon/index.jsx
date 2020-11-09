f72fc10dac27818259692b7f8fa4a9a0ce899bf9
victor martins <victor.martins-terceiro@cea.com.br>
2020-11-09 19:32:26 -0300

    Correções menores

--- src/components/pokemon/index.jsx
+++ src/components/pokemon/index.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 import styled from 'styled-components'
 
 const MISSGNOIMG = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/0.png"
-const PokemonImg = styled.img``
+const PokemonImg = styled.img`
+  cursor: pointer;
+`
 
 export default function Pokemon (props) {
   return <li onClick={() => props.selectPokemon(props)}>
